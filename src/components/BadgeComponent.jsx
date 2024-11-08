6d5eed9fac68535fb95dc824b7b0d69a5a304214
SAITEJA KOMIRISHETTY <komirishettysaiteja@gmail.com>
2024-11-08 23:56:31 +0530

    Fiexd naming issue

--- src/components/BadgeComponent.jsx
+++ src/components/BadgeComponent.jsx
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { useEffect, useRef, useState } from 'react';
 import { Canvas, extend, useThree, useFrame } from '@react-three/fiber';
 import bandImage from '../assets/image.png';
-import Idcard from '../assets/Idcard.png';
+import IdCard from '../assets/Idcard.png';
 import {
     useGLTF,
     useTexture,
@@ -92,7 +92,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
         'https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/5huRVDzcoDwnbgrKUo1Lzs/53b6dd7d6b4ffcdbd338fa60265949e1/tag.glb'
     );
     const texture = useTexture(bandImage);
-    const IdCardTexture = useTexture(Idcard);
+    const IdCardTexture = useTexture(IdCard);
     const { width, height } = useThree((state) => state.size);
     const [curve] = useState(
         () =>
