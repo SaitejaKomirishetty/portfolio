6b435816a305a8e0c0bd9cb8f11d886f6902cb9d
SAITEJA KOMIRISHETTY <komirishettysaiteja@gmail.com>
2024-12-02 21:26:47 +0530

    added flooting doc color in light mode

--- src/components/ui/floating-dock.jsx
+++ src/components/ui/floating-dock.jsx
@@ -89,7 +89,7 @@ const FloatingDockDesktop = ({ items, className }) => {
             onMouseMove={(e) => mouseX.set(e.pageX)}
             onMouseLeave={() => mouseX.set(Infinity)}
             className={cn(
-                'mx-auto hidden md:flex h-16 gap-4 items-end  rounded-2xl bg-gray-50 dark:bg-neutral-900 px-4 pb-3',
+                'mx-auto hidden md:flex h-16 gap-4 items-end  rounded-2xl bg-stone-100 dark:bg-neutral-900 px-4 pb-3',
                 className
             )}
         >
